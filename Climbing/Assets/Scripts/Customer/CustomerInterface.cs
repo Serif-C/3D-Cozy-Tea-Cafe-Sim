194ec549d06550c3889b81ef7a53cf34d8f83a4d
Serif-C <92169062+Serif-C@users.noreply.github.com>
2025-10-06 22:52:21 -0400

    Customer Movement script, ITarget, and IMover interfaces

---
+++ Climbing/Assets/Scripts/Customer/CustomerInterface.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+
+public interface ITarget
+{
+    Vector3 Position { get; }
+}
+
+public interface IMover
+{
+    bool IsMoving { get; }
+    event Action ReachedTarget;
+    void GoTo(ITarget target);
+    void Stop();
+    void Warp(Vector3 position);
+}
--- Climbing/Assets/Scripts/Customer/CustomerMovement.cs
+++ Climbing/Assets/Scripts/Customer/CustomerMovement.cs
@@ -1,9 +1,43 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class CustomerMovement : MonoBehaviour
+public class CustomerMovement : MonoBehaviour, IMover
 {
-    /*
-     * This script is responsible for:
-     * 1. Controlling the customer's ability to move around in the cafe
-     */
+    [SerializeField] private float moveSpeed = 2f;
+    private NavMeshAgent agent;
+
+    public bool IsMoving    // same as `IsMoving =>agent.hasPath && agent.remainingDistance > agent.stoppingDistance;`
+    {
+        get
+        {
+            return agent.hasPath && agent.remainingDistance > agent.stoppingDistance;
+        }
+    }
+
+    public event Action ReachedTarget;
+
+    public void GoTo(ITarget target)
+    {
+        agent.isStopped = false;
+        agent.SetDestination(target.Position);
+    }
+
+    public void Stop()
+    {
+        agent.isStopped = true;
+        agent.ResetPath();
+    }
+
+    public void Warp(Vector3 position)
+    {
+        agent.Warp(position);
+    }
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
 }

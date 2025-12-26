dd4fc790db8d3a6105cc4918c8be5ea728cd8a77
Serif-C <92169062+Serif-C@users.noreply.github.com>
2025-12-25 20:15:12 -0500

    Refactoring MenuItems to support an  Ingredients System.

---
+++ Tea Cafe/Assets/Scripts/Systems/Order System/Recipe.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe : MonoBehaviour
+{
+    /*
+     * Lets say each menu item has a recipe associated with it.
+     * A recipe can be simple or complex, depending on the item.
+     * A simple recipe requires just a few ingredients and readily available tools.
+     * A complex recipe requires more ingredients and specialized tools (can be bought and unlocked later in the game).
+     */
+
+    public Recipe(MainDish)
+    {
+        
+    }
+}
